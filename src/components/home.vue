680c3a534f4316f968e537d790d434ca3a6bae13
zwq <766324531@qq.com>
2020-06-19 11:13:40 +0800

    第二次登录提交

---
+++ src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>home组件
+  <el-button type="primary" @click="exit">退出</el-button>
+  </div>
+</template>
+
+<script>
+  export default{
+    methods:{
+      exit(){
+        window.sessionStorage.clear();
+        this.$router.push('/login');
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
